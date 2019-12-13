a07e1db295b2000e8146bffc7178be210e7d657a
borisponomarenko <56125357+borisponomarenko@users.noreply.github.com>
2019-12-14 01:22:28 +0300

    Add files via upload

---
+++ testba.java
@@ -0,0 +1,11 @@
+package lab2;
+
+import java.lang.*;
+public class testba {
+    public static void main(String[] args){
+        ba b1 = new ba(100, 100);
+        System.out.println(b1);
+        b1.move(30, 15);
+        System.out.println(b1);
+    }
+}
